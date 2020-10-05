46a4405a28e2873ebc172c66aefc1612515e1d20
Conrad Storz <conradstorz@gmail.com>
2020-10-05 12:30:35 -0400

    added ability to form sub-diectories automatcally

--- filehandling.py
+++ filehandling.py
@@ -4,6 +4,7 @@
 """
 
 from pathlib import Path
+from loguru import logger
 
 # set your reference point with the location of the python file you’re writing in
 this_file = Path(__file__)
@@ -12,6 +13,7 @@ this_file = Path(__file__)
 this_folder1 = Path(__file__, "..")
 this_folder2 = Path(__file__) / ".."
 this_folder3 = Path(__file__).parent
+
 # This will fail becasue the variables are relative paths:
 # assert this_folder1 == this_folder2 == this_folder3
 
@@ -68,24 +70,49 @@ this_folder3 = Path(__file__).parent
 """
 
 
+@logger.catch
 def clean_filename_str(fn):
     """Remove invalid characters from provided string."""
-    return Path("".join(i for i in fn if i not in "\/:*?<>|"))
+    # TODO replace invalid characters with underscores
+    return "".join(i for i in fn if i not in "\/:*?<>|")
 
 
-def check_and_validate(fname, direc, rename=True):
+@logger.catch
+def create_datebased_subdirectory_Structure(timestamp: str):
+    """Takes a string (2020-10-05_020600UTC) representing a datetime 
+    and attempts to create a directory structure 
+    in the format ./YYYY/MM/DD/ and returns a Pathobj to the directory.
+    """
+    date, _time = timestamp.split('_') # split date from time
+    yy, mm, dd = date.split('-')
+    OP = f'{yy}/{mm}/{dd}/'
+    return OP
+
+
+@logger.catch
+def check_and_validate(fname, direc, rename=True, use_subdirs=False):
     """Return a PathObj for this filename/directory.
     Fail if filename already exists in directory but optionally rename.
     """
-    fn = clean_filename_str(fname)
+    clean_fn = clean_filename_str(fname)
     direc.mkdir(parents=True, exist_ok=True)
-    OUT_PATH_HANDLE = Path(direc, fn)
+    if use_subdirs:
+        dest = create_datebased_subdirectory_Structure(clean_fn)
+    else:
+        dest = ""
+    OUT_PATH = Path(direc, dest)
     i = 0
-    while Path(OUT_PATH_HANDLE).exists():
+    while Path(OUT_PATH, clean_fn).exists():
         if rename:
+            # TODO  strip old (#) from name and rename
             i += 1
-            fn = f"{fn}({i})"
-            OUT_PATH_HANDLE = Path(direc, fn)
+            clean_fn = f"{clean_fn}({i})"
         else:
-            raise (FileExistsError)
-    return OUT_PATH_HANDLE
+            raise FileExistsError
+    return Path(OUT_PATH, clean_fn)
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    this_file = Path(__file__)
+    print(f'This file {this_file} has no current standalone function.')
