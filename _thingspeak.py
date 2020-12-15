627d2ec538a3be35e47a5288842c05f6d6752aa8
Conrad Storz <conradstorz@gmail.com>
2020-12-15 13:11:31 -0500

    reformat with black

--- _thingspeak.py
+++ _thingspeak.py
@@ -10,23 +10,22 @@ import requests
 import pandas as pd
 
 
-channel1 = '1216774'
-channel2 = '1239835'
+channel1 = "1216774"
+channel2 = "1239835"
 channels = [channel1, channel2]
 
-url_str_base = 'https://api.thingspeak.com/channels/'
-url_str_tail = '/feeds.json?days=2'
+url_str_base = "https://api.thingspeak.com/channels/"
+url_str_tail = "/feeds.json?days=2"
 
 url_list = []
 for channel in channels:
-    url = f'{url_str_base}{channel}{url_str_tail}'
+    url = f"{url_str_base}{channel}{url_str_tail}"
     url_list.append(url)
 
 
 @logger.catch
 def load_json_data_into_dict(url):
-    """Return a dict from a JSON source
-    """
+    """Return a dict from a JSON source"""
     # TODO check for bad url response
     r = requests.get(url)
     j = r.json()
@@ -35,38 +34,35 @@ def load_json_data_into_dict(url):
 
 @logger.catch
 def split_dicts_from_raw_data(json_dict):
-    """Split channels and feeds dicts.
-    """
-    jcd = json_dict['channel'] # equals a dict
-    print(f'channel is type:{type(jcd)}')
-    jfd = json_dict['feeds'] # equals a list of dicts
-    print(f'feeds is type:{type(jfd)}')
+    """Split channels and feeds dicts."""
+    jcd = json_dict["channel"]  # equals a dict
+    print(f"channel is type:{type(jcd)}")
+    jfd = json_dict["feeds"]  # equals a list of dicts
+    print(f"feeds is type:{type(jfd)}")
     return (jcd, jfd)
 
 
 @logger.catch
 def label_feeds_dict_items_with_fieldnames(c_dict, f_dict_list):
-    """Rename feeds fields with channel descriptive names.
-    """
+    """Rename feeds fields with channel descriptive names."""
     new_list = []
     for measurement in f_dict_list:
         new_measurement = {}
         for k1, v1 in measurement.items():
             new_measurement[k1] = v1
-            if k1 != 'created_at': # don't change measurement date
-                if k1 in c_dict.keys(): 
+            if k1 != "created_at":  # don't change measurement date
+                if k1 in c_dict.keys():
                     # copy the descriptive name from the channel into the feed
                     new_measurement[c_dict[k1]] = v1
                     # remove non-descriptive entry
                     del new_measurement[k1]
-        new_list.append(new_measurement)        
+        new_list.append(new_measurement)
     return new_list
 
 
 @logger.catch
 def feeds_dict_update(url):
-    """Copy descriptive names of fields into feeds dict.
-    """
+    """Copy descriptive names of fields into feeds dict."""
     json_feed = load_json_data_into_dict(url)
     channel, feeds = split_dicts_from_raw_data(json_feed)
     updated_feeds = label_feeds_dict_items_with_fieldnames(channel, feeds)
@@ -77,15 +73,15 @@ def feeds_dict_update(url):
 def load_data_into_pandas(list_of_dicts):
     df = pd.DataFrame(list_of_dicts)
     # remove "entry_id" field
-    df = df.drop(['entry_id'], axis=1)
+    df = df.drop(["entry_id"], axis=1)
     # convert datestring to datetime object
-    df["created_at"]=pd.to_datetime(df['created_at'], format="%Y-%m-%dT%H:%M:%SZ")
+    df["created_at"] = pd.to_datetime(df["created_at"], format="%Y-%m-%dT%H:%M:%SZ")
     columns = list(df.columns)
-    columns.remove('created_at')
+    columns.remove("created_at")
     # convert just columns that are not 'created_at'
     df[columns] = df[columns].apply(pd.to_numeric)
     # set 'created_at' as index
-    df.set_index('created_at', inplace = True)
+    df.set_index("created_at", inplace=True)
     return df
 
 
