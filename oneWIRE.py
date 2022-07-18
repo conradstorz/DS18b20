dbec00982d419afb7c7688bfac39044f7675c2a6
Conrad Storz <conradstorz@gmail.com>
2022-07-18 10:09:04 -0400

    limited precision to two decimals

--- oneWIRE.py
+++ oneWIRE.py
@@ -78,7 +78,8 @@ def retreive_device_names():
 def calculate_temp(raw, min=-29, max=260):
     """Accepts raw output of DS18b20 sensor.
     Minimum and Maximum values can be specified to filter out noisy bad results.
-    Returns a tuple of temp in C and F.
+    Returns a tuple of temp in C and F. 
+    Output truncated at 2 decimals precision.
     """
     # TODO sanitize and validate min/max variables against bad user input.
     equals_pos = -1
@@ -87,11 +88,11 @@ def calculate_temp(raw, min=-29, max=260):
         # logger.info(f'Equals position: {equals_pos}')
     if equals_pos != -1:
         temp_string = raw[1][equals_pos + 2 :]
-        temp_c = float(temp_string) / 1000.0        
+        temp_c = round(float(temp_string) / 1000.0, 2)        
         if temp_c < float(min): temp_c = float(min)
         if temp_c > float(max): temp_c = float(max)
         logger.info(f'Temperature string: {temp_string}')
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        temp_f = round(temp_c * 9.0 / 5.0 + 32.0, 2)
         return (temp_c, temp_f)
     return (0,0)
 
